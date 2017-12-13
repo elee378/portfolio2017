c02c44fda31eb9a13a4b723b77ca45225ec2443e
elee378 <ericjaelee@hotmail.com>
2017-12-13 16:38:14 -0500

    first styling
    Styling the index page

---
+++ js/contentObject.js
@@ -0,0 +1,81 @@
+var dynamicContent = {
+		all : {
+			text : "Shrimp is the fruit of the sea, you can broil it, boiol it, bake it, saute it.",
+
+			headline : "all",
+
+			images : [
+				"all_1.jpg",
+				"all_2.jpg",
+				"all_3.jpg",
+				"all_4.jpg"
+			],
+
+			imageDescription : [
+				"this is description 13",
+				"this is description 14",
+				"this is description 15",
+				"this is description 16"
+			]
+		},
+		
+		web: {
+			text : "Dey's uh, shrimp-kabobs, shrimp creole, shrimp gumbo. Pan fried, deep fried, stir-fried.",
+
+			headline : "web",
+
+			images : [
+				"spring_1.jpg",
+				"spring_2.jpg",
+				"spring_3.jpg",
+				"spring_4.jpg"
+			],
+
+			imageDescription : [
+				"this is description 1",
+				"this is description 2",
+				"this is description 3",
+				"this is description 4"
+			]
+		},
+
+		design : {
+			text : "There's pineapple shrimp, lemon shrimp, coconut shrimp, pepper shrimp, shrimp soup",
+
+			headline : "design",
+
+			images : [
+				"summer_1.jpg",
+				"summer_2.jpg",
+				"summer_3.jpg",
+				"summer_4.jpg"
+			],
+
+			imageDescription : [
+				"this is description 5",
+				"this is description 6",
+				"this is description 7",
+				"this is description 8"
+			]
+		},
+
+		other : {
+			text : "shrimp stew, shrimp salad, shrimp and potatoes, shrimp burger, shrimp sandwich. I think thats all the shrimp.",
+
+			headline : "other",
+
+			images : [
+				"autumn_1.jpg",
+				"autumn_2.jpg",
+				"autumn_3.jpg",
+				"autumn_4.jpg"
+			],
+
+			imageDescription : [
+				"this is description 9",
+				"this is description 10",
+				"this is description 11",
+				"this is description 12"
+			]
+		}
+	};
